107f3aada3f5d6b36b1d8de78d59623708849c64
Pedro Cattori <pcattori@gmail.com>
2019-01-10 14:08:36 -0500

    Include `long_description` for PyPI

--- setup.py
+++ setup.py
@@ -6,10 +6,14 @@ project_root = os.path.dirname(__file__)
 with open(os.path.join(project_root, "VERSION.txt")) as f:
     version = f.readline().rstrip()
 
+with open(os.path.join(project_root, "README.md"), "r") as f:
+    long_description = f.read()
+
 setup(
     name="tamr_unify_client",
     version=version,
     description="Python Client for the Tamr Unify API",
+    long_description=long_description,
     url="https://github.com/Datatamer/unify-python-client",
     maintainer="Pedro Cattori",
     maintainer_email="pedro.cattori@tamr.com",
