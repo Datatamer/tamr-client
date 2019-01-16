748e324f493fb46224b45af5a72e0fafafa38d70
Pedro Cattori <pcattori@gmail.com>
2019-01-16 12:41:10 -0500

    Unit test to check that client surfaces HTTP errors as exceptions

---
+++ tests/unit/test_http_error.py
@@ -0,0 +1,19 @@
+import responses
+from requests.exceptions import HTTPError
+import pytest
+
+from tamr_unify_client.auth import UsernamePasswordAuth
+from tamr_unify_client import Client
+
+
+@responses.activate
+def test_http_error():
+    """Ensure that the client surfaces HTTP errors as exceptions.
+    """
+    endpoint = f"http://localhost:9100/api/versioned/v1/projects/1"
+    responses.add(responses.GET, endpoint, status=401)
+    auth = UsernamePasswordAuth("nonexistent-username", "invalid-password")
+    unify = Client(auth)
+    with pytest.raises(HTTPError) as e:
+        project = unify.projects.by_resource_id("1")
+    assert f"401 Client Error: Unauthorized for url: {endpoint}" in str(e)
