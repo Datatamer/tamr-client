23cde6e9e2960630bd584a5f07e25a40f8a47d96
Andy Lee <andy.lee@tamr.com>
2019-02-12 22:36:20 -0500

    Add /status subresource to dataset

---
+++ tamr_unify_client/models/dataset_status.py
@@ -0,0 +1,33 @@
+from tamr_unify_client.models.base_resource import BaseResource
+
+
+class DatasetStatus(BaseResource):
+    """Streamability status of a Unify dataset."""
+
+    @classmethod
+    def from_json(cls, client, resource_json, api_path=None) -> "DatasetStatus":
+        return super().from_data(client, resource_json, api_path)
+
+    @property
+    def dataset_name(self) -> str:
+        """The name of the associated dataset.
+
+        :type: str
+        """
+        return self.data["datasetName"]
+
+    @property
+    def relative_dataset_id(self) -> str:
+        """The relative dataset ID of the associated dataset.
+
+        :type: str
+        """
+        return self.data["relativeDatasetId"]
+
+    @property
+    def is_streamable(self) -> bool:
+        """Whether the associated dataset is available to be streamed.
+
+        :type: bool
+        """
+        return self.data["isStreamable"]
---
+++ tests/unit/test_dataset_status.py
@@ -0,0 +1,27 @@
+import json
+
+import responses
+
+from tamr_unify_client import Client
+from tamr_unify_client.auth import UsernamePasswordAuth
+
+status_json = {
+    "datasetName": "ds1",
+    "relativeDatasetId": "v1/datasets/1",
+    "isStreamable": True,
+}
+
+
+@responses.activate
+def test_dataset_status():
+    dataset_id = "1"
+    dataset_url = f"http://localhost:9100/api/versioned/v1/datasets/{dataset_id}"
+    status_url = f"{dataset_url}/status"
+    responses.add(responses.GET, dataset_url, json={})
+    responses.add(responses.GET, status_url, json=status_json)
+    auth = UsernamePasswordAuth("username", "password")
+    unify = Client(auth)
+
+    dataset = unify.datasets.by_resource_id(dataset_id)
+    status = dataset.status()
+    assert status.data == status_json
