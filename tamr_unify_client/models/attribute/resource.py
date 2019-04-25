7d8e40bc3b21e9c21a10174e23606b02b094a6a8
Nik Bates-Haus <nik.bates-haus@tamr.com>
2019-04-25 17:23:22 -0400

    Document api_path / relative_id nuances.

--- tamr_unify_client/models/attribute/resource.py
+++ tamr_unify_client/models/attribute/resource.py
@@ -15,6 +15,20 @@ class Attribute(BaseResource):
 
     @property
     def relative_id(self):
+        """:type: str"""
+        # api_path is alias when it exists, and relative_id when it does not.
+        # this distinction is useful for things like refreshing a unified dataset,
+        # where using the relative_id would hit
+        #   /datasets/{id}:refresh
+        # rather than
+        #   /projects/{id}/unifiedDataset:refresh.
+        # Since attributes don't currently have that kind of aliasing,
+        # using api_path is always correct.
+        # If attributes ever get aliased, this will need to be updated.
+        # This is confusing; there's an RFC for suggestions to improve this
+        # #64 https://github.com/Datatamer/unify-client-python/issues/64
+        # "Conflation between 'api_path', 'relative_id' / 'relativeId', and
+        # BaseResource ctor 'alias'"
         return self.api_path
 
     @property
