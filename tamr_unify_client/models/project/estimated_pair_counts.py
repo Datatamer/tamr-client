defd46adeeed683b567861c7c57cb7b19d6bc953
mollysacks <43151005+mollysacks@users.noreply.github.com>
2019-06-13 10:58:27 -0400

    more docstring changes

--- tamr_unify_client/models/project/estimated_pair_counts.py
+++ tamr_unify_client/models/project/estimated_pair_counts.py
@@ -20,7 +20,12 @@ class EstimatedPairCounts(BaseResource):
     def total_estimate(self) -> dict:
         """The total number of estimated candidate pairs and generated pairs for the model across all clauses.
 
-        :return: Candidate pairs and estimated pairs with corresponding estimated counts
+        :return: A dictionary containing candidate pairs and estimated pairs mapped to their corresponding estimated counts
+                For example: 
+                {
+                    "candidatePairCount": "54321",
+                    "generatedPairCount": "12345"
+                }
         :rtype: dict[str, int]
         """
         return self._data.get("totalEstimate")
@@ -29,8 +34,19 @@ class EstimatedPairCounts(BaseResource):
     def clause_estimates(self) -> dict:
         """The estimated candidate pair count and generated pair count for each clause in the model.
 
-        :return: Each clause with corresponding estimated candidate pair count and generated pair count.
-        :rtype: dict[str, [dict[str, int]]
+        :return: A dictionary containing each clause name mapped to a dictionary containing the corresponding estimated candidate and generated pair counts
+                For example:
+                  {
+                    "Clause1": {
+                      "candidatePairCount": "321",
+                      "generatedPairCount": "123"
+                    },
+                    "Clause2": {
+                      "candidatePairCount": "654",
+                      "generatedPairCount": "456"
+                    }
+                  }
+        :rtype: dict[str, dict[str, int]]
         """
         return self._data.get("clauseEstimates")
 
