0c572e5de5fcf2da31902cff792dd767ed6f4e72
mollysacks <43151005+mollysacks@users.noreply.github.com>
2019-06-20 11:38:37 -0400

    change method and variable names

--- tamr_unify_client/models/project/collection.py
+++ tamr_unify_client/models/project/collection.py
@@ -62,17 +62,17 @@ class ProjectCollection(BaseCollection):
         """
         return super().stream(Project)
 
-    def create_project(self, project_creation_spec):
+    def create(self, creation_spec):
         """
         Create a Project in Unify
 
-        :param project_creation_spec: Project creation specification should be formatted as specified in the `Public Docs for Creating a Project <https://docs.tamr.com/reference#create-a-project>`_.
-        :type project_creation_spec: dict[str, str]
+        :param creation_spec: Project creation specification should be formatted as specified in the `Public Docs for Creating a Project <https://docs.tamr.com/reference#create-a-project>`_.
+        :type creation_spec: dict[str, str]
         :returns: The created Project
         :rtype: :class:`~tamr_unify_client.models.project.resource.Project`
         """
         data = (
-            self.client.post(self.api_path, json=project_creation_spec)
+            self.client.post(self.api_path, json=creation_spec)
             .successful()
             .json()
         )
