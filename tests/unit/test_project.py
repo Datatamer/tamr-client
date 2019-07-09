5704506646219773422a438b324ae9e286fac1fb
Nik Bates-Haus <nik.bates-haus@tamr.com>
2019-07-09 11:39:42 -0400

    Prefactor: move test_project_by_external_id to test_project

--- tests/unit/test_project.py
+++ tests/unit/test_project.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 
+import pytest
 import responses
 
 from tamr_unify_client import Client
@@ -29,6 +30,22 @@ class TestProject(TestCase):
         input_datasets = project.client.get(alias).successful().json()
         assert input_datasets == self.dataset_json
 
+    @responses.activate
+    def test_project_by_external_id__raises_when_not_found(self):
+        responses.add(responses.GET, self.projects_url, json=[])
+        auth = UsernamePasswordAuth("username", "password")
+        unify = Client(auth)
+        with pytest.raises(KeyError):
+            unify.projects.by_external_id(self.project_external_id)
+
+    @responses.activate
+    def test_project_by_external_id_succeeds(self):
+        responses.add(responses.GET, self.projects_url, json=self.project_json)
+        auth = UsernamePasswordAuth("username", "password")
+        unify = Client(auth)
+        actual_project = unify.projects.by_external_id(self.project_external_id)
+        assert actual_project._data == self.project_json[0]
+
     dataset_external_id = "1"
     datasets_url = f"http://localhost:9100/api/versioned/v1/datasets?filter=externalId=={dataset_external_id}"
     dataset_json = [
@@ -57,7 +74,7 @@ class TestProject(TestCase):
     project_json = [
         {
             "id": "unify://unified-data/v1/projects/1",
-            "externalId": "1",
+            "externalId": "project 1 external ID",
             "name": "project 1 name",
             "description": "project 1 description",
             "type": "DEDUP",
@@ -75,7 +92,7 @@ class TestProject(TestCase):
             "relativeId": "projects/1",
         }
     ]
-    project_external_id = "1"
+    project_external_id = "project 1 external ID"
     projects_url = f"http://localhost:9100/api/versioned/v1/projects?filter=externalId=={project_external_id}"
     post_input_datasets_json = []
     input_datasets_url = (
