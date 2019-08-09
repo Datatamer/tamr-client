9a787da6539a5cb2a680af23019a53b55794c351
juliamcclellan <julia.mcclellan@tamr.com>
2019-08-09 14:19:53 -0400

    attribute builder

--- tamr_unify_client/attribute/resource.py
+++ tamr_unify_client/attribute/resource.py
@@ -1,3 +1,5 @@
+import copy
+
 from tamr_unify_client.attribute.type import AttributeType
 from tamr_unify_client.base_resource import BaseResource
 
@@ -52,10 +54,62 @@ class Attribute(BaseResource):
         """:type: bool"""
         return self._data.get("isNullable")
 
+    def builder(self):
+        """Returns a builder instance to modify this attribute.
+
+        :return: A builder with the same data as this attribute.
+        :rtype: :class:`~tamr_unify_client.attribute.resource.AttributeBuilder`
+        """
+        return AttributeBuilder(self)
+
     def __repr__(self):
         return (
             f"{self.__class__.__module__}."
             f"{self.__class__.__qualname__}("
             f"relative_id={self.relative_id!r}, "
             f"name={self.name!r})"
+        )
+
+
+class AttributeBuilder:
+    """A builder object to modify an existing attribute.
+
+    :param attribute: The attribute to modify.
+    :type attribute: :class:`~tamr_unify_client.attribute.resource.Attribute`
+    """
+
+    def __init__(self, attribute):
+        self._data = copy.deepcopy(attribute._data)
+        self.client = attribute.client
+        self.api_path = attribute.api_path
+
+    def with_description(self, new_description):
+        """Updates the description of the attribute.
+
+        :param new_description: The updated description.
+        :type new_description: str
+        :return: This attribute builder.
+        :rtype: :class:`~tamr_unify_client.attribute.resource.AttributeBuilder`
+        """
+        self._data["description"] = new_description
+        return self
+
+    def put(self):
+        """Commits the changes and updates the attribute in Tamr.
+
+        :return: The updated attribute.
+        :rtype: :class:`~tamr_unify_client.attribute.resource.Attribute`
+        """
+        updated_attribute = (
+            self.client.put(self.api_path, json=self._data).successful().json()
+        )
+        return Attribute.from_json(self.client, updated_attribute, self.api_path)
+
+    def __repr__(self):
+        return (
+            f"{self.__class__.__module__}."
+            f"{self.__class__.__qualname__}("
+            f"relative_id={self.api_path!r}, "
+            f"name={self._data['name']!r}, "
+            f"description={self._data['description']!r})"
         )
