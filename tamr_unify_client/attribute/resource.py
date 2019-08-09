a32c32be7a9e29e789dd3bdc478b9123a7c82e8e
juliamcclellan <julia.mcclellan@tamr.com>
2019-08-09 14:19:53 -0400

    attribute builder

--- tamr_unify_client/attribute/resource.py
+++ tamr_unify_client/attribute/resource.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 from tamr_unify_client.attribute.type import AttributeType
 from tamr_unify_client.base_resource import BaseResource
 
@@ -52,10 +54,88 @@ class Attribute(BaseResource):
         """:type: bool"""
         return self._data.get("isNullable")
 
+    def spec(self):
+        """Returns a spec representation of this attribute.
+
+        :return: The attribute spec.
+        :rtype: :class:`~tamr_unify_client.attribute.resource.AttributeSpec`
+        """
+        return AttributeSpec.of(self)
+
     def __repr__(self):
         return (
             f"{self.__class__.__module__}."
             f"{self.__class__.__qualname__}("
             f"relative_id={self.relative_id!r}, "
             f"name={self.name!r})"
+        )
+
+
+class AttributeSpec:
+    """A representation of the server view of an attribute"""
+
+    def __init__(self, client, data, api_path):
+        self._data = data
+        self.client = client
+        self.api_path = api_path
+
+    @staticmethod
+    def of(resource):
+        """Creates an attribute spec from an attribute.
+
+        :param resource: The existing attribute.
+        :type resource: :class:`~tamr_unify_client.attribute.resource.Attribute`
+        :return: The corresponding attribute spec.
+        :rtype: :class:`~tamr_unify_client.attribute.resource.AttributeSpec`
+        """
+        return AttributeSpec(
+            resource.client, deepcopy(resource._data), resource.api_path
+        )
+
+    def from_data(self, data):
+        """Creates a spec with the same client and API path as this one, but new data.
+
+        :param data: The data for the new spec.
+        :type data: dict
+        :return: The new spec.
+        :rtype: :class:`~tamr_unify_client.attribute.resource.AttributeSpec`
+        """
+        return AttributeSpec(self.client, data, self.api_path)
+
+    def to_dict(self):
+        """Returns a version of this spec that conforms to the API representation.
+
+        :returns: The spec's dict.
+        :rtype: dict
+        """
+        return deepcopy(self._data)
+
+    def with_description(self, new_description):
+        """Creates a new spec with the same properties, updating description.
+
+        :param new_description: The new description.
+        :type new_description: str
+        :return: The new spec.
+        :rtype: :class:`~tamr_unify_client.attribute.resource.AttributeSpec`
+        """
+        return self.from_data({**self._data, "description": new_description})
+
+    def put(self):
+        """Commits the changes and updates the attribute in Tamr.
+
+        :return: The updated attribute.
+        :rtype: :class:`~tamr_unify_client.attribute.resource.Attribute`
+        """
+        updated_attribute = (
+            self.client.put(self.api_path, json=self._data).successful().json()
+        )
+        return Attribute.from_json(self.client, updated_attribute, self.api_path)
+
+    def __repr__(self):
+        return (
+            f"{self.__class__.__module__}."
+            f"{self.__class__.__qualname__}("
+            f"relative_id={self.api_path!r}, "
+            f"name={self._data['name']!r}, "
+            f"description={self._data['description']!r})"
         )
