09dbd82e2c9b0c11a375dc281c3a9e36b608a668
juliamcclellan <julia.mcclellan@tamr.com>
2019-08-13 11:16:37 -0400

    testing attribute configuration builder

--- tests/unit/test_attribute_configuration.py
+++ tests/unit/test_attribute_configuration.py
@@ -1,3 +1,5 @@
+from functools import partial
+import json
 from unittest import TestCase
 
 from requests import HTTPError
@@ -57,22 +59,57 @@ class TestAttributeConfiguration(TestCase):
 
     @responses.activate
     def test_delete(self):
-        base = "http://localhost:9100/api/versioned/v1"
-        alias = "projects/1/attributeConfigurations"
-        attribute_id = "26"
-
-        url = f"{base}/{alias}/{attribute_id}"
+        url = f"{self._base}/{self._alias}/{self._attribute_id}"
         responses.add(responses.GET, url, json=self._ac_json)
         responses.add(responses.DELETE, url, status=204)
         responses.add(responses.GET, url, status=404)
 
-        collection = AttributeConfigurationCollection(self.tamr, alias)
-        config = collection.by_resource_id(attribute_id)
+        collection = AttributeConfigurationCollection(self.tamr, self._alias)
+        config = collection.by_resource_id(self._attribute_id)
+
         self.assertEqual(config._data, self._ac_json)
 
         response = config.delete()
         self.assertEqual(response.status_code, 204)
-        self.assertRaises(HTTPError, lambda: collection.by_resource_id(attribute_id))
+        self.assertRaises(
+            HTTPError, lambda: collection.by_resource_id(self._attribute_id)
+        )
+
+    @responses.activate
+    def test_update(self):
+        def create_callback(request, snoop):
+            snoop["payload"] = request.body
+            return 200, {}, json.dumps(self._updated_ac_json)
+
+        configs_url = f"{self._base}/{self._alias}"
+        config_url = f"{configs_url}/{self._attribute_id}"
+
+        snoop_dict = {}
+        responses.add(responses.GET, config_url, json=self._ac_json)
+        responses.add_callback(
+            responses.PUT, config_url, partial(create_callback, snoop=snoop_dict)
+        )
+        configs = AttributeConfigurationCollection(self.tamr, self._alias)
+        config = configs.by_resource_id(self._attribute_id)
+
+        temp_spec = config.spec().with_attribute_role("SUM_ATTRIBUTE")
+        new_config = (
+            temp_spec.with_enabled_for_ml(False)
+            .with_similarity_function("ABSOLUTE_DIFF")
+            .with_tokenizer("BIGRAM")
+            .put()
+        )
+
+        self.assertEqual(new_config._data, self._updated_ac_json)
+        self.assertEqual(json.loads(snoop_dict["payload"]), self._updated_ac_json)
+        self.assertEqual(config._data, self._ac_json)
+
+        # checking that intermediate didn't change
+        self.assertTrue(temp_spec.to_dict()["enabledForMl"])
+
+    _base = "http://localhost:9100/api/versioned/v1"
+    _alias = "projects/1/attributeConfigurations"
+    _attribute_id = "26"
 
     _ac_json = {
         "id": "unify://unified-data/v1/projects/1/attributeConfigurations/26",
@@ -82,6 +119,18 @@ class TestAttributeConfiguration(TestCase):
         "similarityFunction": "COSINE",
         "enabledForMl": True,
         "tokenizer": "DEFAULT",
+        "numericFieldResolution": [],
+        "attributeName": "surname",
+    }
+
+    _updated_ac_json = {
+        "id": "unify://unified-data/v1/projects/1/attributeConfigurations/26",
+        "relativeId": "projects/1/attributeConfigurations/26",
+        "relativeAttributeId": "datasets/8/attributes/surname",
+        "attributeRole": "SUM_ATTRIBUTE",
+        "similarityFunction": "ABSOLUTE_DIFF",
+        "enabledForMl": False,
+        "tokenizer": "BIGRAM",
         "numericFieldResolution": [],
         "attributeName": "surname",
     }
