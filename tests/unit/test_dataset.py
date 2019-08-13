cb584bae667a619e5b26f709822dab249f44208f
juliamcclellan <julia.mcclellan@tamr.com>
2019-08-13 11:52:20 -0400

    test dataset builder

--- tests/unit/test_dataset.py
+++ tests/unit/test_dataset.py
@@ -1,3 +1,5 @@
+from functools import partial
+import json
 from unittest import TestCase
 
 from requests import HTTPError
@@ -14,10 +16,9 @@ class TestAttribute(TestCase):
 
     @responses.activate
     def test_delete(self):
-        url = "http://localhost:9100/api/versioned/v1/datasets/1"
-        responses.add(responses.GET, url, json=self._dataset_json)
-        responses.add(responses.DELETE, url, status=204)
-        responses.add(responses.GET, url, status=404)
+        responses.add(responses.GET, self._url, json=self._dataset_json)
+        responses.add(responses.DELETE, self._url, status=204)
+        responses.add(responses.GET, self._url, status=404)
 
         dataset = self.tamr.datasets.by_resource_id("1")
         self.assertEqual(dataset._data, self._dataset_json)
@@ -25,6 +26,40 @@ class TestAttribute(TestCase):
         response = dataset.delete()
         self.assertEqual(response.status_code, 204)
         self.assertRaises(HTTPError, lambda: self.tamr.datasets.by_resource_id("1"))
+
+    @responses.activate
+    def test_update(self):
+        def create_callback(request, snoop):
+            snoop["payload"] = request.body
+            return 200, {}, json.dumps(self._updated_dataset_json)
+
+        snoop_dict = {}
+        responses.add(responses.GET, self._url, json=self._dataset_json)
+        responses.add_callback(
+            responses.PUT, self._url, partial(create_callback, snoop=snoop_dict)
+        )
+
+        dataset = self.tamr.datasets.by_resource_id("1")
+
+        temp_spec = dataset.spec().with_description(
+            self._updated_dataset_json["description"]
+        )
+        new_dataset = (
+            temp_spec.with_external_id(self._updated_dataset_json["externalId"])
+            .with_tags(self._updated_dataset_json["tags"])
+            .put()
+        )
+
+        self.assertEqual(new_dataset._data, self._updated_dataset_json)
+        self.assertEqual(json.loads(snoop_dict["payload"]), self._updated_dataset_json)
+        self.assertEqual(dataset._data, self._dataset_json)
+
+        # checking that intermediate didn't change
+        self.assertEqual(
+            temp_spec.to_dict()["externalId"], self._dataset_json["externalId"]
+        )
+
+    _url = "http://localhost:9100/api/versioned/v1/datasets/1"
 
     _dataset_json = {
         "id": "unify://unified-data/v1/datasets/1",
@@ -34,6 +69,28 @@ class TestAttribute(TestCase):
         "version": "dataset 1 version",
         "keyAttributeNames": ["tamr_id"],
         "tags": [],
+        "created": {
+            "username": "admin",
+            "time": "2018-09-10T16:06:20.636Z",
+            "version": "dataset 1 created version",
+        },
+        "lastModified": {
+            "username": "admin",
+            "time": "2018-09-10T16:06:20.851Z",
+            "version": "dataset 1 modified version",
+        },
+        "relativeId": "datasets/1",
+        "upstreamDatasetIds": [],
+    }
+
+    _updated_dataset_json = {
+        "id": "unify://unified-data/v1/datasets/1",
+        "externalId": "dataset1",
+        "name": "dataset 1 name",
+        "description": "updated description",
+        "version": "dataset 1 version",
+        "keyAttributeNames": ["tamr_id"],
+        "tags": ["new", "tags"],
         "created": {
             "username": "admin",
             "time": "2018-09-10T16:06:20.636Z",
