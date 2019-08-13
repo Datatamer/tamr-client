e83d51870266752888201b1e5a69c830323c5680
juliamcclellan <julia.mcclellan@tamr.com>
2019-08-13 11:01:19 -0400

    attribute configuration builder

--- tamr_unify_client/project/attribute_configuration/resource.py
+++ tamr_unify_client/project/attribute_configuration/resource.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 from tamr_unify_client.base_resource import BaseResource
 
 
@@ -58,6 +60,14 @@ class AttributeConfiguration(BaseResource):
         """:type: str"""
         return self._data.get("attributeName")
 
+    def spec(self):
+        """Returns this attribute configuration's spec.
+
+         :return: The spec of this attribute configuration.
+         :rtype: :class:`~tamr_unify_client.project.attribute_configuration.resource.AttributeConfigurationSpec`
+         """
+        return AttributeConfigurationSpec.of(self)
+
     def __repr__(self):
         return (
             f"{self.__class__.__module__}."
@@ -71,4 +81,122 @@ class AttributeConfiguration(BaseResource):
             f"tokenizer={self.tokenizer!r}, "
             f"numeric_field_resolution={self.numeric_field_resolution!r}, "
             f"attribute_name={self.attribute_name!r})"
+        )
+
+
+class AttributeConfigurationSpec:
+    """A representation of the server view of an attribute configuration."""
+
+    def __init__(self, client, data, api_path):
+        self.client = client
+        self._data = data
+        self.api_path = api_path
+
+    @staticmethod
+    def of(resource):
+        """Creates an attribute configuration spec from an attribute configuration.
+
+        :param resource: The existing attribute configuration.
+        :type resource: :class:`~tamr_unify_client.project.attribute_configuration.resource.AttributeConfiguration`
+        :return: The corresponding attribute creation spec.
+        :rtype: :class:`~tamr_unify_client.project.attribute_configuration.resource.AttributeConfigurationSpec`
+        """
+        return AttributeConfigurationSpec(
+            resource.client, deepcopy(resource._data), resource.api_path
+        )
+
+    def from_data(self, data):
+        """Creates a spec with the same client and API path as this one, but new data.
+
+        :param data: The data for the new spec.
+        :type data: dict
+        :return: The new spec.
+        :rtype: :class:`~tamr_unify_client.project.attribute_configuration.resource.AttributeConfigurationSpec`
+        """
+        return AttributeConfigurationSpec(self.client, data, self.api_path)
+
+    def to_dict(self):
+        """Returns a version of this spec that conforms to the API representation.
+
+        :returns: The spec's dict.
+        :rtype: dict
+        """
+        return deepcopy(self._data)
+
+    def with_attribute_role(self, new_attribute_role):
+        """Creates a new spec with the same properties, updating attribute role.
+
+        :param new_attribute_role: The new attribute role.
+        :type new_attribute_role: str
+        :return: A new spec.
+        :rtype: :class:`~tamr_unify_client.project.attribute_configuration.resource.AttributeConfigurationSpec`
+        """
+        return self.from_data({**self._data, "attributeRole": new_attribute_role})
+
+    def with_similarity_function(self, new_similarity_function):
+        """Creates a new spec with the same properties, updating similarity function.
+
+        :param new_similarity_function: The new similarity function.
+        :type new_similarity_function: str
+        :return: A new spec.
+        :rtype: :class:`~tamr_unify_client.project.attribute_configuration.resource.AttributeConfigurationSpec`
+        """
+        return self.from_data(
+            {**self._data, "similarityFunction": new_similarity_function}
+        )
+
+    def with_enabled_for_ml(self, new_enabled_for_ml):
+        """Creates a new spec with the same properties, updating enabled for ML.
+
+        :param new_enabled_for_ml: Whether the builder is enabled for ML.
+        :type new_enabled_for_ml: bool
+        :return: A new spec.
+        :rtype: :class:`~tamr_unify_client.project.attribute_configuration.resource.AttributeConfigurationSpec`
+        """
+        return self.from_data({**self._data, "enabledForMl": new_enabled_for_ml})
+
+    def with_tokenizer(self, new_tokenizer):
+        """Creates a new spec with the same properties, updating tokenizer.
+
+        :param new_tokenizer: The new tokenizer.
+        :type new_tokenizer: str
+        :return: A new spec.
+        :rtype: :class:`~tamr_unify_client.project.attribute_configuration.resource.AttributeConfigurationSpec`
+        """
+        return self.from_data({**self._data, "tokenizer": new_tokenizer})
+
+    def with_numeric_field_resolution(self, new_numeric_field_resolution):
+        """Creates a new spec with the same properties, updating numeric field resolution.
+
+        :param new_numeric_field_resolution: The new numeric field resolution.
+        :type new_numeric_field_resolution: str
+        :return: A new spec.
+        :rtype: :class:`~tamr_unify_client.project.attribute_configuration.resource.AttributeConfigurationSpec`
+        """
+        return self.from_data(
+            {**self._data, "numericFieldResolution": new_numeric_field_resolution}
+        )
+
+    def put(self):
+        """Updates the attribute configuration on the server.
+
+        :return: The modified attribute configuration.
+        :rtype: :class:`~tamr_unify_client.project.attribute_configuration.resource.AttributeConfiguration`
+        """
+        new_data = self.client.put(self.api_path, json=self._data).successful().json()
+        return AttributeConfiguration.from_json(self.client, new_data, self.api_path)
+
+    def __repr__(self):
+        return (
+            f"{self.__class__.__module__}."
+            f"{self.__class__.__qualname__}("
+            f"relative_id={self._data['relativeId']!r}, "
+            f"id={self._data['id']!r}, "
+            f"relative_attribute_id={self._data['relativeAttributeId']!r}, "
+            f"attribute_role={self._data['attributeRole']!r}, "
+            f"similarity_function={self._data['similarityFunction']!r}, "
+            f"enabled_for_ml={self._data['enabledForMl']!r}, "
+            f"tokenizer={self._data['tokenizer']!r}, "
+            f"numeric_field_resolution={self._data['numericFieldResolution']!r}, "
+            f"attribute_name={self._data['attributeName']!r})"
         )
