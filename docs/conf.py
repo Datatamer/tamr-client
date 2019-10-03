bbf541b28a68b09645f65277a67fef872c34a8c1
Pedro Cattori <pcattori@gmail.com>
2019-10-02 21:34:34 -0400

    feat(docs): sphinx.ext.napoleon for Google-style docstrings
    Replaces sphinx.ext.autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ release = version
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.intersphinx", "sphinx.ext.viewcode"]
+extensions = ["sphinx.ext.napoleon", "sphinx.ext.intersphinx", "sphinx.ext.viewcode"]
 autodoc_default_flags = ["inherited-members", "members"]
 autodoc_member_order = "bysource"
 intersphinx_mapping = {
