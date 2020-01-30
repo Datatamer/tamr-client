f8e37f3b22a5ba6ef046b90cbbcc332e7d1016d2
Pedro Cattori <pcattori@gmail.com>
2020-01-29 22:54:44 -0500

    Dataset module tests

---
+++ tests/datasets/test_dataset.py
@@ -0,0 +1,32 @@
+from dataclasses import replace
+
+from requests import Session
+import responses
+
+import tamr_unify_client as tc
+from tamr_unify_client.auth import UsernamePasswordAuth
+from tamr_unify_client.dataset.resource import Dataset
+from tests.utils import data_dir, load_json
+
+
+@responses.activate
+def test__attributes():
+    auth = UsernamePasswordAuth("admin", "dt")
+    tamr = tc.Client(auth)
+    dataset_json = load_json(data_dir / "dataset.json")
+    dataset_url = tc.URL(path="api/versioned/v1/datasets/1")
+    dataset = Dataset.from_json(tamr, dataset_json, api_path=dataset_url.path)
+
+    attrs_url = replace(dataset.url, path=dataset.url.path + "/attributes")
+    attrs_json = load_json(data_dir / "attributes.json")
+    responses.add(responses.GET, str(attrs_url), json=attrs_json, status=204)
+
+    attrs = tc.dataset._attributes(Session(), dataset)
+
+    row_num = attrs[0]
+    assert row_num.name == "RowNum"
+    assert isinstance(row_num.type, tc.attribute_type.String)
+
+    geom = attrs[1]
+    assert geom.name == "geom"
+    assert isinstance(geom.type, tc.attribute_type.Record)
