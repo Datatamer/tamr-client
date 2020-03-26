88a8dea42341afae5f89d51be59a0db9d98e3bf4
skalish <samk825@gmail.com>
2020-03-26 11:27:37 -0400

    Move shared helper functions to utils.py.

--- tests/tamr_client/datasets/test_dataframe.py
+++ tests/tamr_client/datasets/test_dataframe.py
@@ -1,5 +1,4 @@
 from functools import partial
-import json
 from typing import Dict
 
 import pandas as pd
@@ -16,17 +15,21 @@ def test_upsert():
     dataset = utils.dataset()
 
     url = tc.URL(path="datasets/1:updateRecords")
-    updates = _records_to_updates(_records_json)
+    updates = utils.records_to_updates(_records_json)
     snoop: Dict = {}
     responses.add_callback(
-        responses.POST, str(url), partial(_capture_payload, snoop=snoop, status=200)
+        responses.POST,
+        str(url),
+        partial(
+            utils.capture_payload, snoop=snoop, status=200, response_json=_response_json
+        ),
     )
 
     df = pd.DataFrame(_records_json)
 
     response = tc.dataframe.upsert(s, dataset, df, primary_key_name="primary_key")
     assert response == _response_json
-    assert snoop["payload"] == _stringify(updates)
+    assert snoop["payload"] == utils.stringify(updates)
 
 
 @responses.activate
@@ -38,26 +41,6 @@ def test_upsert_primary_key_not_found():
 
     with pytest.raises(tc.record.PrimaryKeyNotFound):
         tc.dataframe.upsert(s, dataset, df, primary_key_name="wrong_primary_key")
-
-
-def _capture_payload(request, snoop, status):
-    """Capture request body within `snoop` so we can inspect that the request body is constructed correctly (e.g. for streaming requests).
-
-    See https://github.com/getsentry/responses#dynamic-responses
-    """
-    snoop["payload"] = list(request.body)
-    return status, {}, json.dumps(_response_json)
-
-
-def _records_to_updates(records):
-    return [
-        {"action": "CREATE", "recordId": i, "record": record}
-        for i, record in enumerate(records, start=1)
-    ]
-
-
-def _stringify(updates):
-    return [json.dumps(u) for u in updates]
 
 
 _records_json = [{"primary_key": 1}, {"primary_key": 2}]
