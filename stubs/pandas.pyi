4d7af83e18f2c9289160b285a2cbdb801082e619
skalish <samk825@gmail.com>
2020-04-01 16:01:56 -0400

    Add pandas stubfile.

---
+++ stubs/pandas.pyi
@@ -0,0 +1,19 @@
+from typing import Any, Dict, Iterator, List, Optional, Tuple
+
+JsonDict = Dict[str, Any]
+
+class DataFrame:
+    columns: Index
+
+    def __init__(
+            self,
+            data: List[JsonDict] = None,
+    ): ...
+
+    def iterrows(self) -> Iterator[Tuple[int, Series]]: ...
+
+class Series:
+    def to_json(self) -> str: ...
+
+class Index:
+    def __iter__(self) -> Iterator[str]: ...
