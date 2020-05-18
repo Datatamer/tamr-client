096c0a917b13b5539f43495d15eceaac72d882e6
Ian Bakst <ianbakst@gmail.com>
2020-05-18 12:50:33 -0400

    I think this should lint correctly.

--- tamr_client/dataset/__init__.py
+++ tamr_client/dataset/__init__.py
@@ -1,9 +1,8 @@
+# flake8: noqa
 # See https://github.com/python/mypy/issues/5759
 from tamr_client.dataset.dataset import Dataset
 
-from tamr_client.dataset.dataset import (
-    from_resource_id,
-)
+from tamr_client.dataset.dataset import from_resource_id
 from tamr_client.dataset.dataset import DatasetNotFound
 import tamr_client.dataset.dataframe as dataframe
 import tamr_client.dataset.record as record
