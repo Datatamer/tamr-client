79e414ae153311bde4d354b01517f6eccd6f62d2
ianbakst <38843235+ianbakst@users.noreply.github.com>
2020-05-27 13:37:12 -0400

    change import so we don't have a problem with 3.6

--- tamr_client/dataset/__init__.py
+++ tamr_client/dataset/__init__.py
@@ -1,7 +1,5 @@
 # flake8: noqa
 from tamr_client.dataset.dataset import Dataset
-
 from tamr_client.dataset.dataset import from_resource_id
 from tamr_client.dataset.dataset import DatasetNotFound
-import tamr_client.dataset.dataframe as dataframe
-import tamr_client.dataset.record as record
+from tamr_client.dataset import dataframe, record
