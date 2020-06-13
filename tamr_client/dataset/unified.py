39a419f7a979e06514db4d0853645e4a72eaefba
Ian Bakst <ianbakst@gmail.com>
2020-06-12 21:06:38 -0400

    linter again

--- tamr_client/dataset/unified.py
+++ tamr_client/dataset/unified.py
@@ -7,9 +7,9 @@ from typing import Optional, Tuple
 
 from tamr_client import response
 from tamr_client.instance import Instance
+from tamr_client.project import Project
 from tamr_client.session import Session
 from tamr_client.types import JsonDict
-from tamr_client.project import Project
 from tamr_client.url import URL
 
 
