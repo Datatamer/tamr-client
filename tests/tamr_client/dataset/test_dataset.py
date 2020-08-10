3f53d8f835c6aaa171dc4f2c438d1822b5bf9a70
skalish <samk825@gmail.com>
2020-08-10 10:32:03 -0400

    Update basic dataset testing.

--- tests/tamr_client/dataset/test_dataset.py
+++ tests/tamr_client/dataset/test_dataset.py
@@ -7,14 +7,10 @@ import tamr_client as tc
 from tests.tamr_client import fake, utils
 
 
-@responses.activate
+@fake.json
 def test_from_resource_id():
     s = fake.session()
     instance = fake.instance()
-
-    dataset_json = utils.load_json("dataset.json")
-    url = tc.URL(path="datasets/1")
-    responses.add(responses.GET, str(url), json=dataset_json)
 
     dataset = tc.dataset.from_resource_id(s, instance, "1")
     assert dataset.name == "dataset 1 name"
@@ -22,13 +18,10 @@ def test_from_resource_id():
     assert dataset.key_attribute_names == ("tamr_id",)
 
 
-@responses.activate
+@fake.json
 def test_from_resource_id_dataset_not_found():
     s = fake.session()
     instance = fake.instance()
-
-    url = tc.URL(path="datasets/1")
-    responses.add(responses.GET, str(url), status=404)
 
     with pytest.raises(tc.dataset.NotFound):
         tc.dataset.from_resource_id(s, instance, "1")
