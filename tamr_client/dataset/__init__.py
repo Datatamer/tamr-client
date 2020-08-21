47cce86a5e4c0f351b85f442671f5a5186a0d113
Ian Bakst <ianbakst@gmail.com>
2020-08-21 13:47:50 -0400

    removed hoist of dataset.Ambiguous exception

--- tamr_client/dataset/__init__.py
+++ tamr_client/dataset/__init__.py
@@ -1,7 +1,6 @@
 from tamr_client.dataset import dataframe, record, unified
 from tamr_client.dataset._dataset import (
     _refresh_async,
-    Ambiguous,
     attributes,
     from_resource_id,
     NotFound,
