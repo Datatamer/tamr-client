e4a1a7760aa784e64f9157cec5695879c6daed13
Ian Bakst <ianbakst@gmail.com>
2020-08-21 13:41:18 -0400

    added testing

--- tamr_client/dataset/__init__.py
+++ tamr_client/dataset/__init__.py
@@ -1,8 +1,9 @@
 from tamr_client.dataset import dataframe, record, unified
-from tamr_client.dataset._dataset import(
+from tamr_client.dataset._dataset import (
+    _refresh_async,
+    Ambiguous,
     attributes,
     from_resource_id,
     NotFound,
-    Ambiguous,
     refresh,
 )
