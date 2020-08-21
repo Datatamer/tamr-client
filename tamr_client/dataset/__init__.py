660949e8275e6a8e60e9853a67443de0b5335b5e
Ian Bakst <ianbakst@gmail.com>
2020-08-21 11:52:15 -0400

    add refresh to dataset.__init__.py

--- tamr_client/dataset/__init__.py
+++ tamr_client/dataset/__init__.py
@@ -1,2 +1,7 @@
 from tamr_client.dataset import dataframe, record, unified
-from tamr_client.dataset._dataset import attributes, from_resource_id, NotFound
+from tamr_client.dataset._dataset import(
+    attributes,
+    from_resource_id,
+    NotFound,
+    refresh,
+)
