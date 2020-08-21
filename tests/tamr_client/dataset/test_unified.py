3be8d08796314662e9bf1ebb4ca71fe1e01147f3
skalish <samk825@gmail.com>
2020-08-21 10:32:03 -0400

    Update tc.unified tests.

--- tests/tamr_client/dataset/test_unified.py
+++ tests/tamr_client/dataset/test_unified.py
@@ -1,18 +1,13 @@
 import pytest
-import responses
 
 import tamr_client as tc
-from tests.tamr_client import fake, utils
+from tests.tamr_client import fake
 
 
-@responses.activate
+@fake.json
 def test_from_project():
     s = fake.session()
     project = fake.mastering_project()
-
-    dataset_json = utils.load_json("dataset.json")
-    url = tc.URL(path="projects/1/unifiedDataset")
-    responses.add(responses.GET, str(url), json=dataset_json)
 
     unified_dataset = tc.dataset.unified.from_project(s, project)
     assert unified_dataset.name == "dataset 1 name"
@@ -20,29 +15,26 @@ def test_from_project():
     assert unified_dataset.key_attribute_names == ("tamr_id",)
 
 
-@responses.activate
+@fake.json
 def test_from_project_dataset_not_found():
     s = fake.session()
     project = fake.mastering_project()
-
-    url = tc.URL(path="projects/1/unifiedDataset")
-    responses.add(responses.GET, str(url), status=404)
 
     with pytest.raises(tc.dataset.unified.NotFound):
         tc.dataset.unified.from_project(s, project)
 
 
-@responses.activate
-def test_apply_changes():
+@fake.json
+def test_apply_changes_async():
     s = fake.session()
-    dataset_json = utils.load_json("dataset.json")
-    dataset_url = tc.URL(path="projects/1/unifiedDataset")
-    unified_dataset = tc.dataset.unified._from_json(dataset_url, dataset_json)
+    unified_dataset = fake.unified_dataset()
 
-    operation_json = utils.load_json("operation_pending.json")
-    operation_url = tc.URL(path="operations/1")
-    url = tc.URL(path="projects/1/unifiedDataset:refresh")
-    responses.add(responses.POST, str(url), json=operation_json)
-
-    response = tc.dataset.unified._apply_changes_async(s, unified_dataset)
-    assert response == tc.operation._from_json(operation_url, operation_json)
+    op = tc.dataset.unified._apply_changes_async(s, unified_dataset)
+    assert op.type == "SPARK"
+    assert op.description == "operation 1 description"
+    assert op.status == {
+        "state": "PENDING",
+        "startTime": "",
+        "endTime": "",
+        "message": "Job has not yet been submitted to Spark",
+    }
